f76dcd941a9a64ecbf8a49a84e41311b4addaf3c
birkan kolcu <ResearcherOne@users.noreply.github.com>
2020-07-23 20:14:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 //create a server object:
 http.createServer(function (req, res) {
   console.log("INFO: You are awesome!!!")
-  res.write('You are awesome!!!'); //write a response to the client
+  res.write('Everyone is awesomeeee with CI/CD!!!'); //write a response to the client
   res.end(); //end the response
 }).listen(PORT, function(err, res){
     console.log("Listeninng!!");
